3ecc2343769d499db5295e69d9853326814c286a
imvenx <imvenx@proton.me>
2023-10-06 19:33:33 +0200

    add guitar etc

--- SceneController.gd
+++ SceneController.gd
@@ -3,6 +3,14 @@ extends WorldEnvironment
 var padQuaternion = Quat()
 onready var meshChild = get_child(0)
 
+onready var audioPlayer:AudioStreamPlayer = $AudioPlayer
+var swordSound = load("res://sounds/sword2.mp3")
+var batSound = load("res://sounds/bat.mp3")
+var guitarSound = load("res://sounds/guitar.mp3")
+var rockMusic = load("res://sounds/rock.mp3")
+var gunSound = load("res://sounds/gun.mp3")
+
+
 func _ready():
 #	var initParams = { 'arcaneCode': '0.30' }
 	Arcane.init()
@@ -10,6 +18,39 @@ func _ready():
 
 	yield(get_tree().create_timer(4.0), "timeout")
 	$CSGMesh/Phone.hide()
+	$CSGMesh/sword.show()
+	
+	yield(get_tree().create_timer(2.0), "timeout")
+	playSound(swordSound)
+	
+	yield(get_tree().create_timer(2.0), "timeout")
+	$CSGMesh/sword.hide()
+	$CSGMesh/bat_joined.show()
+	yield(get_tree().create_timer(2.0), "timeout")
+	playSound(batSound)
+	
+	yield(get_tree().create_timer(2.0), "timeout")
+	$CSGMesh/bat_joined.hide()
+	
+	
+#	$VideoPlayer.show()
+#	$VideoPlayer.play()
+	
+#	yield(get_tree().create_timer(29), "timeout")
+	$CSGMesh/guitar.show()
+	$BgMusic.stop()
+	
+	yield(get_tree().create_timer(2.0), "timeout")
+	$BgMusic.stream = rockMusic
+	$BgMusic.play()
+	
+	yield(get_tree().create_timer(14), "timeout")
+	playSound(gunSound)
+	
+	yield(get_tree().create_timer(3), "timeout")
+	$CSGMesh/guitar.hide()
+	$CSGMesh/gun.show()
+	
 
 func onArcaneClientInitialized(_e):
 	if not Arcane.pads: return
@@ -39,3 +80,10 @@ func onGetQuaternion(e):
 
 func _process(_delta):
 	meshChild.transform.basis = Basis(padQuaternion)
+	
+
+func playSound(audioStream):
+	audioStream.loop = false
+	audioPlayer.stream = audioStream
+	audioPlayer.play()
+	
